fbb989ecc443ee4af05bdcfbe58846699af646da
Raquel Félix <126718221+raquelphantom@users.noreply.github.com>
2025-09-17 22:11:01 -0300

    Add files via upload

--- endereco_e_malloc.c
+++ endereco_e_malloc.c
@@ -2,22 +2,32 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
-
+int num,i;
+char*vet;
+char letra; 
+	
 char* strichar(char s[],char c){
 	char*vlr = NULL;
+	int i;
+	for(i=0; i<num; i++){
+		
+	}
 }
 
 void pegar_palavra(){
-	int num,i;
-	char*vet;
-	printf("qual tamanho da palavra?");
+	
+	printf("\n qual tamanho da palavra?");
 	fflush(stdin); scanf("%i",&num);
 	//vetor dinâmico
 	vet= malloc((num+1) * sizeof(char));
 	//pega a palavra e coloca no vetor
-	printf("digite uma palavra com até %i letras",num);
+	printf("\ndigite uma palavra com até %i letras:  ",num);
 	fflush(stdin); scanf("%s",vet);
 	printf("%s",vet);
+	
+	//pergunta a letra que quer achar
+	printf("\ninsira a letra que quer encontrar: ");
+	fflush(stdin); scanf("%c", &letra);
 }
 
 int main(){
